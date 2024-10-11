06a6a003019d06aa9d6bc3fcc0572ed655515424
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-10-11 11:37:09 -0300

    w

---
+++ perguntas.js
@@ -0,0 +1,184 @@
+
+criaCartao(
+
+    'matemática',
+
+    'Qual é a fórmula de Pitágoras, e quando ela deve ser usada?',
+
+    ' h²= a² + b². É usada para determinar a medida desconhecida de um lado'
+)
+
+criaCartao(
+
+    'matemática',
+
+    'Quais são os tipos de triângulos, conforme a medida dos lados?',
+
+    ' Equilátero (todos lados iguais), isósceles (dois lados iguais), Escaleno (todos os lados diferentes)'
+
+)
+
+criaCartao(
+
+    'matemática',
+
+    'Quais são os tipos de triângulos, conforme a medida dos ângulos?',
+
+    ' acutângulo (ângulos menores de 90°), Retângulo (ângulo de 90°), obtusângulo(ângulos maiores de 90°)'
+)
+
+criaCartao(
+
+    'matemática',
+
+    'Qual é a fórmula de um arranjo simples e quando deve ser utilizada?',
+
+    'A=n!/(n-p)!. Ele deve ser utilizada quando se quer fazer uma análise combinatória'
+)
+
+criaCartao(
+
+    'Química',
+
+    'Quais são os elementos e/ou grupos que tem o nox fixo?',
+
+    'Al(alumínio), todo o grupo um e dois da tabela periódica, e o F(flúor)'
+)
+
+criaCartao(
+
+    'Química',
+
+    'Quais são os elementos que compõem a pilha de Daniell?',
+
+    'Cu(cobre) e Zn(zinco).'
+)
+
+criaCartao(
+
+    'Química',
+
+    'Seguindo a regra do octeto em ligações químicas, quantos elétrons o elemento deve conter na última camada de valência para ficar estável?',
+
+    '8 elétrons na última camada.'
+)
+
+criaCartao(
+
+    'Química',
+
+    'De acordo com a IUPAC, como deve ser feito a representação oficial de uma pilha?',
+
+    'oxida//reduz (ex. Zn⁰/Zn²//Cu²/Cu⁰).'
+)
+
+criaCartao(
+
+    'Biologia',
+
+    'Quais são os nomes dos super grupos da classificação biológica.',
+
+    'Archea, eubactérias, eucarya.'
+)
+
+criaCartao(
+
+    'Biologia',
+
+    'Quais são os reinos da classificação taxonômica?',
+
+    'Monera, animalia, fungi, protista, e vegetal.'
+)
+
+criaCartao(
+
+    'Biologia',
+
+    'Qual a diferença entre autótrofas e heterótrofas?',
+
+    'Seres autótrofos fazem sua própria comida, enquanto heterotróficas se alimentam de algo da natureza.'
+)
+
+criaCartao(
+
+    'Biologia',
+
+    'Quais são os nomes dos folhetos embrionários?',
+
+    'Ectoderme, endoderme e mesoderme.'
+)
+
+criaCartao(
+
+    'Biologia',
+
+    'Quais são os nomes dos folhetos embrionários?',
+
+    'Ectoderme, endoderme e mesoderme.'
+)
+
+criaCartao(
+
+    'geografia',
+
+    'Qual o nome do modelo japonês de produção, e que conta com ajuda de automação, mais conhecido como “just-in-time”?',
+
+    'Toyotismo.'
+)
+
+criaCartao(
+
+    'geografia',
+
+    'O processo de industrialização no Brasil foi iniciado de maneira mais consolidada em que momento da história?',
+
+    'No Governo Vargas, com criação de empresas com infraestrutura.'
+)
+
+criaCartao(
+
+    'história',
+
+    'Qual foi o estopim para o início da segunda guerra mundial?.',
+
+    'O estopim para o início da guerra foi a invasão da Polônia pelos alemães em 1º de setembro de 1939.'
+)
+
+criaCartao(
+
+    'história',
+
+    'o que foram os conhecidos nomes Hiroshima e Nagasaki?',
+
+    'Hiroshima e Nagasaki são os nomes de duas cidades japonesas que foram atacadas com bombas atômicas (usadas pela primeira vez no mundo) lançadas pelos Estados Unidos.'
+)
+
+criaCartao(
+
+    'história',
+
+    'Qual foi o motivo do ataque dos Estados Unidos ao Japão na segunda guerra mundial?',
+
+    'Foi para que eles se rendesse aos Estados Unidos sem que eles precisassem invadir o território japonês por terra.'
+)
+
+criaCartao(
+
+    'história',
+
+    'O que foi a guerra fria? ',
+
+    'Foi um período marcado por um conflito político e ideológico travado entre Estados Unidos e ex-união soviética (URSS)'
+)
+
+criaCartao(
+
+    'história',
+
+    'Qual foi o motivo da vinda dos portugueses ao Brasil na época da colonização?',
+
+    'Vieram para explorar a produção de açúcar que era a atividade mais rentável da colônia no século XVI e XVII, em geral estavam em busca de investimento lucrativo e com mão de obra barata'
+
+)
+
+
