c3d8194c7207ed2e62d7497ba64786a553e9b613
juliaolicavalcante <160850509+juliaolicavalcante@users.noreply.github.com>
2024-10-18 12:05:16 -0300

    fe

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let respostaEstaVisivel = false
     function viraCartao(){
         respostaEstaVisivel = !respostaEstaVisivel;
-        cartao.classList.toggle('active')
+        cartao.classList.toggle('active', respostaEstaVisivel)
 }
 container.appendChild(cartao);
 }
